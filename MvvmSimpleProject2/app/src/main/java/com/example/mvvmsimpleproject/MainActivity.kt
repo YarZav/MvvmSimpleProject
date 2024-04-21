1724c26cac452e6fdaffb29966ea3edf5e530b6b
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2024-04-21 13:55:03 +0300

    Added layout

--- MvvmSimpleProject2/app/src/main/java/com/example/mvvmsimpleproject/MainActivity.kt
+++ MvvmSimpleProject2/app/src/main/java/com/example/mvvmsimpleproject/MainActivity.kt
@@ -2,15 +2,6 @@ package com.example.mvvmsimpleproject
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mvvmsimpleproject.ui.theme.MvvmSimpleProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
